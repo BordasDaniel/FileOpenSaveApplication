739f6ee0070c337369f3fa2a0d60a73e2fcf4835
Dániel Bordás <bordas.daniel0124@gmail.com>
2025-02-10 08:13:06 +0100

    Auto class létrehozva propertykkel és konstruktorokkal. Fájl beolvasás hiba kezelésekkel. autok.txt szöveges dokumentum létrehozva

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,17 +8,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Collections.Generic;
+using FileOpenSaveApplication.Models;
 
 namespace FileOpenSaveApplication
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Auto> autok = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            string allomany = "autok.txt";
+            Beolvas(allomany);
+        }
+
+        private void Beolvas(string allomany)
+        {
+            try
+            {
+                using (StreamReader olvas = new(allomany))
+                {
+                    olvas.ReadLine();
+                    string sor;
+
+                    while ((sor = olvas.ReadLine()) != null)
+                    {
+                        autok.Add(new Models.Auto(sor));
+                    }
+                }
+                MessageBox.Show("Sikeres beolvasás!");
+            }
+            catch (FormatException fex)
+            {
+                MessageBox.Show($"Hibás adatformátum {fex.Message}!");
+            }
+
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show($"A fájl nem található: {allomany}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hiba történt: {ex.Message}");
+            }
         }
     }
 }
---
+++ Models/Auto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOpenSaveApplication.Models
+{
+    internal class Auto
+    {
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public string Tipus { get; set; }
+        public string Szin { get; set; }
+        public int Evjarat { get; set; }
+        public DateOnly Muszaki { get; set; }
+        public bool Aktiv { get; set; }
+
+        public Auto() { }
+
+        public Auto(int id, string marka, string tipus, string szin, int evjarat, DateOnly muszaki, bool aktiv)
+        {
+            Id = id;
+            Marka = marka;
+            Tipus = tipus;
+            Szin = szin;
+            Evjarat = evjarat;
+            Muszaki = muszaki;
+            Aktiv = aktiv;
+        }
+
+        public Auto(string AdatSor)
+        {
+            string[] Adatok = AdatSor.Split(';');
+            Id = int.Parse(Adatok[0]);
+            Marka = Adatok[1];
+            Tipus = Adatok[2];
+            Szin = Adatok[3];
+            Evjarat = int.Parse(Adatok[4]);
+            Muszaki = DateOnly.Parse(Adatok[5]);
+            Aktiv = bool.Parse(Adatok[6]);
+        }
+
+
+    }
+}
